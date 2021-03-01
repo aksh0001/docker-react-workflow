e88c044cad6d22272ac2111bd74f76bd70e5c2eb
akshay <akshay.k1738@gmail.com>
2021-02-28 18:21:44 -0600

    Update App render text with break line

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This simple react app is used to demo a full prod workflow!
-          Stack: React app running inside a container => Travis for CI/CD, deploying to AWS EBS (Elastic Beanstalk)
+          <br />
+          Stack: React app running inside an nginx container => Travis for CI/CD, deploying to AWS EBS (Elastic Beanstalk)
         </p>
         <a
           className="App-link"
